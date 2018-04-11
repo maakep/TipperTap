667d3b083c8e0dc0e23ddd844f3b20962a9950ce
Maakep <schon.fredrik@gmail.com>
2018-04-11 21:18:24 +0200

    Initial commit without socket and server

---
+++ src/game/index.tsx
@@ -0,0 +1,8 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import Game from "./game";
+
+ReactDOM.render(
+  <Game />,
+  document.getElementById("root") as HTMLElement,
+);
